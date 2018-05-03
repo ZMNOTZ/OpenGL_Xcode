3235e6c077dfcda0bacc032bb22465410e6c6350
emery <609026421@qq.com>
2018-05-04 00:12:29 +0800

    添加SOIL

--- BaseOpenGL/main.cpp
+++ BaseOpenGL/main.cpp
@@ -8,15 +8,23 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "hello_triangle.h"
+extern "C" {
 #include "SOIL2/SOIL2.h"
+}
 #include "res/shaders/shader.h"
 #include "res/shaders/shader_sample.h"
 int main(int argc, const char * argv[]) {
     
    // hello_triangle_main();
     
-    shader_sample_main();
-    
+   // shader_sample_main();
+
+    int width=200;
+    int height=100;
+    unsigned char* image = SOIL_load_image("/Users/emery/AppcodeProjects/OpenGL_Xcode/BaseOpenGL/res/image/tips.png",
+            &width, &height, 0, SOIL_LOAD_RGBA);
+
+    std::cout<<&image<<std::endl;
     return 0;
     
 }
