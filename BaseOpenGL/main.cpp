6d3ee66dccf5feaa9f4b5888a3948387fa54b5d5
emery <609026421@qq.com>
2018-05-16 00:09:25 +0800

    添加光照 投光物（caster)

--- BaseOpenGL/main.cpp
+++ BaseOpenGL/main.cpp
@@ -19,7 +19,11 @@
 //#include "lighting_color_view_space.h"
 //#include "lighting_color_gouraud.h"
 //#include "lighting_basic.h"
-#include "lighting_texture_diffuse.h"
+//#include "lighting_texture_diffuse.h"
+//#include "lighting_texture_emission.h"
+//#include "lighting_caster_directional.h"
+//#include "lighting_caster_attenuation.h"
+#include "lighting_caster_flashlight.h"
 int main(int argc, const char * argv[]) {
     
    // hello_triangle_main();
@@ -47,8 +51,10 @@ int main(int argc, const char * argv[]) {
     //lighting_color_view_space_main();
     //lighting_color_gouraud_main();
     //lighting_basic_main();
-    lighting_texture_diffuse_main();
-
+    //lighting_texture_diffuse_main();
+    //lighting_texture_emission_main();
+    //lighting_caster_main();
+    lighting_caster_flashlight_main();
     return 0;
     
 }
